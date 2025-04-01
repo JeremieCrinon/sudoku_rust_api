f24b935e4efa6be0db5b2e4ee63309e1676acabf
JeremieCrinon <accounts@jcrinon.com>
2025-04-01 10:41:50 +0200

    Fixing CORS errors.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,21 @@
 pub mod api;
 
-use axum::Router;
+use axum::{
+    Router,
+    http::Method,
+};
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::handlers::api::not_found;
 
 pub fn create_router() -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin(Any) 
+        .allow_methods([Method::POST])
+        .allow_headers(Any);
+
     return Router::new()
         .merge(api::api_routes())
-        .fallback(not_found);
+        .fallback(not_found)
+        .layer(cors);
 }
